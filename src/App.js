43ab8b822f3ffbcdaec10c2ed85a80d9a528bc7f
Muhammad Yasir <sardary015@gmail.com>
2023-08-30 18:09:14 +0500

    fourth commit/ authentication

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Navbar } from "../src/components/navbar";
-import {foooter} from "../src/components/footer"
+// import {foooter} from "../src/components/footer"
 import {Shop} from '../src/pages/shop/shop';
 import {Cart} from '../src/pages/cart/cart';
-import {allProduct} from '../src/pages/shop/allProduct';
+// import {allProduct} from '../src/pages/shop/allProduct';
 import ShopContextProvider from './context/shop-context';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 function App() {
   return (
     <div className="App">
-      <ShopContextProvider>
+     
+     <ShopContextProvider>
+      <Auth0Provider
+    domain="dev-fcp3oek4zeohjjn0.us.auth0.com"
+    clientId="mtwOzkwehqVqraCX0h0YPnuITK3v9djr"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+      >
+      
       <Router>
 
         {/* navbar shown in every page that's why it is above the routes */}   
@@ -24,10 +35,11 @@ function App() {
           <Route path='/allProducts' element={<allProduct/>}/>
 
         </Routes>
-        <footer/>
+    
 
       </Router>
-
+      </Auth0Provider>
+      
       </ShopContextProvider>
     </div>
   );
