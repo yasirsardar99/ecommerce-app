cfc8733bd40ad9a983a8dea221deece7b1667c58
fahad <mfahad1667@gmail.com>
2023-09-19 16:13:43 +0500

    Add footer, modify some files and add packages

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
-
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { Navbar } from "../src/components/navbar";
-// import {foooter} from "../src/components/footer"
-import {Shop} from '../src/pages/shop/shop';
-import {Cart} from '../src/pages/cart/cart';
+import { Shop } from "../src/pages/shop/shop";
+import { Cart } from "../src/pages/cart/cart";
 // import {allProduct} from '../src/pages/shop/allProduct';
-import ShopContextProvider from './context/shop-context';
-import { Auth0Provider } from '@auth0/auth0-react';
+import ShopContextProvider from "./context/shop-context";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useEffect } from "react";
+import Footer from "./components/footer";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
 
 function App() {
   return (
     <div className="App">
-     
-     <ShopContextProvider>
-      <Auth0Provider
-    domain="dev-fcp3oek4zeohjjn0.us.auth0.com"
-    clientId="mtwOzkwehqVqraCX0h0YPnuITK3v9djr"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-      >
-      
-      <Router>
-
-        {/* navbar shown in every page that's why it is above the routes */}   
-        <Navbar/>
-
-        <Routes>
-          
-          <Route path='/' element={<Shop/>} />
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/allProducts' element={<allProduct/>}/>
-
-        </Routes>
-    
-
-      </Router>
-      </Auth0Provider>
-      
+      <ShopContextProvider>
+        <Auth0Provider
+          domain="dev-fcp3oek4zeohjjn0.us.auth0.com"
+          clientId="mtwOzkwehqVqraCX0h0YPnuITK3v9djr"
+          authorizationParams={{
+            redirect_uri: window.location.origin,
+          }}
+        >
+          <Router>
+            <ScrollToTop />
+            {/* navbar shown in every page that's why it is above the routes */}
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Shop />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/allProducts" element={<allProduct />} />
+            </Routes>
+            <Footer />
+          </Router>
+        </Auth0Provider>
       </ShopContextProvider>
     </div>
   );
--- src/pages/shop/allProduct.jsx
+++ src/pages/shop/allProduct.jsx
@@ -1,20 +1,13 @@
-import React from 'react';
-import {PRODUCTS} from '../../products';
-import {Product} from './product';
-
-
+import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
 
 export const allProduct = () => {
-
-
-
   return (
-    
     <div className="shop my-5">
-      
       <div className="shopTitle">
         <h1>All PRODUCTS</h1>
-{/* d-flex justify-content-between border-dark my-3  */}
+        {/* d-flex justify-content-between border-dark my-3  */}
         {/* thats the main thing where product function works. Will need to understand this with focus */}
 
         <div className="container ">
@@ -23,20 +16,18 @@ export const allProduct = () => {
 
             {/* products name have array of values stored map function extract it one by one */}
 
-            
-              {/* what we gonna say we have complete file of products. So, we map it then we take each object one by one.
+            {/* what we gonna say we have complete file of products. So, we map it then we take each object one by one.
               We then make a function of product and pass a new file with a name of Product and share a prop of product as a new sign name of "data"
               */}
 
-            {PRODUCTS.map(
-              (product)=>(<Product data={product}/>)
-            )}
+            {PRODUCTS.map((product) => (
+              <Product data={product} />
+            ))}
           </div>
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default allProduct;
